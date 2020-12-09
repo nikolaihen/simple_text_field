007ffd02e999a29a162608e54c393f04f618c02f
Nikolai Henriksen <nikolaihenriksen@nikolais-air.lan>
2020-12-09 21:42:10 +0100

    Added SimpleTextField class with example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,117 +1,173 @@
 import 'package:flutter/material.dart';
+import 'package:simple_text_field/simple_text_field.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: ExamplePage()
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class ExamplePage extends StatefulWidget {
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _ExamplePageState createState() => _ExamplePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _ExamplePageState extends State<ExamplePage> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  TextEditingController controller;
+  double textFieldHeight = 50;
+  TextStyle textFieldStyle = TextStyle(
+    fontSize: 16.0
+  );
+  bool isDense = true;
+  bool formValid = false;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    controller = TextEditingController();
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
+      backgroundColor: Colors.grey.shade200,
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('SimpleTextField example'),
+        centerTitle: true,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      body: Container(
+        padding: EdgeInsets.symmetric(
+          horizontal: 40
+        ),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SimpleTextField.regular(
+                hintText: 'Regular',
+                controller: controller,
+                onChanged: (String val) {
+                  setState(() {
+                    formValid = val.length >= 5;
+                  });
+                },
+                validator: (String val) {
+                  if (val.length < 5) {
+                    return 'Must be 5 characters or more!';
+                  }
+                  return null;
+                },
+                validInputIcon: Icons.check_circle,
+                onTap: () => print('Tapped'),
+              ),
+              SizedBox(height: 30),
+              SimpleTextField(
+                hintText: 'Custom',
+                controller: controller,
+                height: textFieldHeight,
+                fillColor: Colors.white,
+                filled: true,
+                onChanged: (String val) {
+                  setState(() {
+                    formValid = val.length >= 5;
+                  });
+                },
+                shadow: [
+                  BoxShadow(
+                    offset: Offset(0, 2),
+                    blurRadius: 5,
+                    spreadRadius: 1,
+                    color: Colors.grey.shade400
+                  )
+                ],
+                prefixIcon: Icons.email,
+                suffixIcon: formValid 
+                    ? Icons.check_circle 
+                    : null,
+                validator: (String val) {
+                  if (val.length < 5) {
+                    return 'Must be 5 characters or more!';
+                  }
+                  return null;
+                },
+                borderRadius: BorderRadius.circular(5),
+                enableClearButton: true,
+                textStyle: textFieldStyle,
+                hintStyle: textFieldStyle,
+                isDense: isDense,
+              ),
+              SizedBox(height: 30),
+              RaisedButton(
+                onPressed: () {
+                  _formKey.currentState.validate();
+                },
+                child: Text('Validate'),
+              ),
+              SizedBox(height: 30),
+              Text('CUSTOM TEXTFIELD CONTROLS'),
+              Slider(
+                value: textFieldHeight,
+                onChanged: (double val) {
+                  setState(() {
+                    textFieldHeight = val;
+                  });
+                },
+                min: 10,
+                max: 150,
+                label: textFieldHeight.round().toString(),
+              ),
+              Text('TextField height: ${textFieldHeight.round()}'),
+              SizedBox(height: 15),
+              Slider(
+                value: textFieldStyle.fontSize,
+                onChanged: (double val) {
+                  setState(() {
+                    textFieldStyle = textFieldStyle.copyWith(
+                      fontSize: val
+                    );
+                  });
+                },
+                min: 8,
+                max: 64,
+                label: textFieldStyle.fontSize.round().toString(),
+              ),
+              Text('TextField font size: ${textFieldStyle.fontSize.round()}'),
+              SizedBox(height: 15),
+              CheckboxListTile(
+                title: Text('TextField isDense: $isDense'),
+                value: isDense, 
+                onChanged: (bool val) {
+                  setState(() {
+                    isDense = val;
+                  });
+                }
+              )
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
   }
 }
